8c7578fbcfa2d7573875b4b3f1c2937f756b3968
Zyablovv <alexsandr.zyablov@gmail.com>
2021-06-14 23:38:06 +0300

    --all

--- js/main.js
+++ js/main.js
@@ -1,15 +1,48 @@
 $(function () {
     $('.slider').slick({
         slidesToShow: 1,
+        slidesToScroll: 1,
         arrows: true,
         asNavFor: '.thumbs'
 
-    })
+    });
     $('.thumbs').slick({
-        slidesToShow: 6,
-        slidesToScroll: 6,
+        slidesToShow: 5,
+        slidesToScroll: 1,
         asNavFor: '.slider',
         dots: false,
-        focusOnSelect: true
-    })
+        arrows: false,
+        focusOnSelect: true,
+    }); 
+    var showChar = 600;
+	var ellipsestext = "...";
+	var moretext = "more";
+	var lesstext = "less";
+	$('.more').each(function() {
+		var content = $(this).html();
+
+		if(content.length > showChar) {
+
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar-1, content.length - showChar);
+
+			var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
+
+			$(this).html(html);
+		}
+
+	});
+
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	}); 
 });
